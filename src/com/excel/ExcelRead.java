fae5d91b3118d3b5bb53d5b63fb7d1499dc7833d
Renisha <renisha24@gmail.com>
2017-10-16 16:09:11 +0530

    Initial commit

---
+++ src/com/excel/ExcelRead.java
@@ -0,0 +1,42 @@
+package com.excel;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+
+
+public class ExcelRead {
+	public static void readXLSFile() throws IOException
+	{
+		InputStream ExcelFileToRead = new FileInputStream("C:/Users/RF048808/Desktop/Kamal/kamal1.xlsx");
+		HSSFWorkbook wb = new HSSFWorkbook(ExcelFileToRead);
+
+		HSSFSheet sheet=wb.getSheetAt(0);
+		HSSFRow row; 
+		HSSFCell cell;
+
+		Iterator rows = sheet.rowIterator();
+
+		while (rows.hasNext())
+		{
+			row=(HSSFRow) rows.next();
+			Iterator cells = row.cellIterator();
+			
+			while (cells.hasNext())
+			{
+				cell=(HSSFCell) cells.next();
+		
+
+			}
+			System.out.println();
+		}
+	
+	}
+}
